e10e64d3f6d7958256f0ca8033bd062626f77e21
Amal Raj <52370866+amalraj33@users.noreply.github.com>
2020-06-22 16:15:16 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './App.css';
-import { Page1 } from './Continer/Page1'
-import { Page2 } from './Continer/Page2'
-import { Page3 } from './Continer/Page3'
+import { Header } from './Continer/Header'
+import { Chartpart } from './Continer/Chartpart'
+import { Piechart } from './Continer/Piechart'
 export const Home = () => {
     return (
         <div className="container" style={{marginLeft:"210px"}}>
-         <Page1/>
-         <Page2/>
-         <Page3/>
+         <Header/>
+         <Chartpart/>
+         <Piechart/>
        </div>
     )
 }
